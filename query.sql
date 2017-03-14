522751654cbb255835ab228a25d51d9dfd9cef4d
YIXIONG CHEN <jack.chenyixiong@gmail.com>
2017-03-14 16:00:39 -0400

    query

---
+++ query.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(*) 
+FROM 
+(SElECT T.uid1, T.uid2
+FROM 
+	(SELECT * FROM R WHERE uid1 < uid2) as T, 
+	(SELECT * FROM R WHERE uid2 < uid1) as S 
+WHERE T.uid1 = S.uid2 AND T.uid2 = S.uid1);
+
